cac856124469d9d03551299178d5d7b974578b74
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2023-11-30 19:40:28 +0200

    added data

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 var cors = require("cors");
 const port = 3000;
 
-const data = require("./json/chat_list.json");
+const list = require("./json/chat_list.json");
+const data = require("./json/data.json");
 
 app.use(express.static(path.join(__dirname, "assets")));
 // Middleware для обработки JSON
@@ -13,29 +14,33 @@ app.use(cors());
 
 // Обработка GET запроса
 app.get("/", (req, res) => {
-  res.json(data["chat-list"]);
+  res.json(list["chat-list"]);
 });
 
 app.get("/room_1", (req, res) => {
-  res.json(data["cabinet-chat-message-show-room_1"]);
+  res.json(list["cabinet-chat-message-show-room_1"]);
 });
 
 app.get("/room_2", (req, res) => {
-  res.json(data["cabinet-chat-message-show-room_2"]);
+  res.json(list["cabinet-chat-message-show-room_2"]);
 });
 
 app.get("/room_3", (req, res) => {
-  res.json(data["cabinet-chat-message-show-room_3"]);
+  res.json(list["cabinet-chat-message-show-room_3"]);
 });
 
 app.get("/room_4", (req, res) => {
-  res.json(data["cabinet-chat-message-show-room_4"]);
+  res.json(list["cabinet-chat-message-show-room_4"]);
 });
 
 app.get("/room_5", (req, res) => {
-  res.json(data["cabinet-chat-message-show-room_5"]);
+  res.json(list["cabinet-chat-message-show-room_5"]);
 });
 
+app.post("/search", (req, res) => {
+  const searchData = req.body.search;
+  res.json(data);
+});
 
 // Слушаем порт
 app.listen(port, () => {
